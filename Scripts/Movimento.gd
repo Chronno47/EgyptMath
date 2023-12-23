d28aacfcdc56c15fc076da789fc0fe2806de0729
Chronno47 <154481354+Chronno47@users.noreply.github.com>
2023-12-23 20:02:27 -0300

    Começando a definir a base #2
    fiz a sala de debug + to tentando ajeitar a camera

--- Scripts/Movimento.gd
+++ Scripts/Movimento.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const MOVE_SPEED = 300.0
-const JUMP_FORCE = -400.0
+const MOVE_SPEED = 150.0
+const JUMP_FORCE = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
