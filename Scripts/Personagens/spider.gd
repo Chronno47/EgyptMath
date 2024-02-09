8f72d1f24cd51e74dff52a2fd6cfa4ae4f3dcd1d
Chronno47 <154481354+Chronno47@users.noreply.github.com>
2024-02-09 20:44:09 -0300

    Aranha funcionando
    ditto.

--- Scripts/Personagens/spider.gd
+++ Scripts/Personagens/spider.gd
@@ -1,77 +1,68 @@
 extends Personagens_Gerais
 
+# Atributos das animações, hitbox e hurtbox
 @onready var animations := $"Animacoes" as AnimatedSprite2D
-@onready var wall_detector_left := $"Wall_Detector_Left" as RayCast2D
-@onready var wall_detector_right := $"Wall_Detector_Right" as RayCast2D
 @onready var hitbox := $"Hitbox Component/Hitbox"
 @onready var hurtbox := $"Hurtbox Component/Hurtbox"
-@onready var ground_detector_left := $"Ground_Detector_Left" as RayCast2D
-@onready var ground_detector_right := $"Ground_Detector_Right" as RayCast2D
-@onready var ceiling_detector_left := $"Ceiling_Detector_Left" as RayCast2D
-@onready var ceiling_detector_right := $"Ceiling_Detector_Right" as RayCast2D
 
+# Vida do personagem
 var health:int = 3
 
-var char_on_ground:bool = false
-var char_on_ceiling:bool = false
-
+# Variável para controlar a gravidade
 var is_gravity_on:bool = true
-var direction := -1
 
+# Direção do movimento (1 para direita, -1 para esquerda)
+var direction := 1
+
+# Vetor de movimento
+var move = Vector2(0,0)
+
+# Variável para controle da rotação
+var rotating:int 
+
+# Função chamada a cada quadro para física do personagem
 func _physics_process(delta: float) -> void:
-	if is_gravity_on:
-		velocity.y += gravity * delta
+	# Se estiver rotacionando, atualiza a rotação gradualmente
+	if rotating:
+		rotation = lerp_angle(rotation, move.angle(), 0.1)
+		rotating -= 1
+		return
 	
-	if char_on_ground && !wall_detector_left.is_colliding() || !wall_detector_right.is_colliding():
-		_walk()
+	# Movimento horizontal e verificação de colisão
+	var col := move_and_collide(move * 60 * delta)
+	if col and col.get_normal().rotated(PI/2).dot(move) < 0.5:
+		# Se houver colisão, rotaciona o personagem
+		rotating = 4
+		move = col.get_normal().rotated(PI/2)
+		return
 	
-	if wall_detector_left.is_colliding() || wall_detector_right.is_colliding():
-		_go_up()
-	
-	if char_on_ceiling:
-		if wall_detector_left.is_colliding() || wall_detector_right.is_colliding():
-			_switch_direction()
-			_walk()
+	# Movimento de rotação para contornar obstáculos
+	var pos := position
+	col = move_and_collide(move.rotated(PI/2) * 15)
+	if not col:
+		for i in 10:
+			position = pos
+			rotate(PI/32)
+			move = move.rotated(PI/32)
+			col = move_and_collide(move.rotated(PI/2) * 15)
+			
+			if col:
+				move = col.get_normal().rotated(PI/2)
+				rotation = move.angle()
+				break
+	else:
+		move = col.get_normal().rotated(PI/2)
+		rotation = move.angle()
 	
 	_set_state()
-	_onground_check()
-	_onceiling_check()
-	move_and_slide()
 
-func _onground_check():
-	if ground_detector_left.is_colliding() && ground_detector_right.is_colliding():
-		char_on_ground = true
-	else:
-		char_on_ground = false
-	
-
-func _onceiling_check():
-	if ceiling_detector_left.is_colliding() && ceiling_detector_right.is_colliding():
-		char_on_ceiling = true
-		is_gravity_on = false
-	else:
-		char_on_ceiling = false
-		is_gravity_on = true
-
-func _walk():
-	velocity.x = direction * (move_speed * 0.60)
-	animations.scale.x = direction
-
-func _switch_direction():
-	direction *= -1
-	hitbox.position.x *= -1
-	hurtbox.position.x *= -1
-
-func _go_up():
-	if !char_on_ceiling:
-		velocity.y = direction * (move_speed * 0.60)
-		animations.scale.y = -direction
-
+# Função para receber dano
 func take_damage(damage:int):
 	health -= damage
 	if health <= 0:
 		queue_free()
 
+# Define o estado do personagem com base na direção do movimento
 func _set_state():
 	var state = "Idle"
 	
@@ -79,3 +70,4 @@ func _set_state():
 		state = "Walking"
 		
 	animations.play(state)
+
